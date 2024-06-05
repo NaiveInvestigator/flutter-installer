bf8221e09869f59ce5cce85ce4e86ee55ba5e8a6
NaiveInvestigator <72654596+NaiveInvestigator@users.noreply.github.com>
2024-06-05 11:03:25 +0600

    feat: make sure uninstall script removes selected lines

--- uninstall.sh
+++ uninstall.sh
@@ -3,26 +3,26 @@
 # Remove environment variable from their respective shells
 if [ -f ~/.bashrc ]; then
     sed -i '/export ANDROID_HOME=$HOME\/Development\/android/d' ~/.bashrc
-    sed -i '/export PATH=$ANDROID_HOME\/cmdline-tools\/latest\/bin/d' ~/.bashrc
-    sed -i '/export PATH=$ANDROID_HOME\/emulator\//d' ~/.bashrc
-    sed -i '/export PATH=$ANDROID_HOME\/platform-tools\//d' ~/.bashrc
-    sed -i '/export PATH=$HOME\/Development\/flutter\/bin\//d' ~/.bashrc
+    sed -i '/export PATH=$ANDROID_HOME\/cmdline-tools\/latest\/bin:$PATH/d' ~/.bashrc
+    sed -i '/export PATH=$ANDROID_HOME\/emulator:$PATH/d' ~/.bashrc
+    sed -i '/export PATH=$ANDROID_HOME\/platform-tools:$PATH/d' ~/.bashrc
+    sed -i '/export PATH=$HOME\/Development\/flutter\/bin:$PATH/d' ~/.bashrc
 fi
-    
+
 if [ -f ~/.zshrc ]; then
     sed -i '/export ANDROID_HOME=$HOME\/Development\/android/d' ~/.zshrc
     sed -i '/export PATH=$ANDROID_HOME\/cmdline-tools\/latest\/bin:$PATH/d' ~/.zshrc
-    sed -i '/export PATH=$ANDROID_HOME\/emulator\/:$PATH/d' ~/.zshrc
-    sed -i '/export PATH=$ANDROID_HOME\/platform-tools\/:$PATH/d' ~/.zshrc
-    sed -i '/export PATH=$HOME\/Development\/flutter\/bin\/:$PATH/d' ~/.zshrc
+    sed -i '/export PATH=$ANDROID_HOME\/emulator:$PATH/d' ~/.zshrc
+    sed -i '/export PATH=$ANDROID_HOME\/platform-tools:$PATH/d' ~/.zshrc
+    sed -i '/export PATH=$HOME\/Development\/flutter\/bin:$PATH/d' ~/.zshrc
 fi
 
 if [ -f ~/.config/fish/config.fish ]; then
-    sed -i 'set -xg PATH "$HOME\/Development\/flutter\/bin/d"' ~/.config/fish/config.fish
-    sed -i 'set -xg ANDROID_HOME "$HOME\/Development\/android/d"' ~/.config/fish/config.fish
-    sed -i 'set -xg PATH "$ANDROID_HOME\/cmdline-tools\/latest\/bin\/d"' ~/.config/fish/config.fish
-    sed -i 'set -xg PATH "$ANDROID_HOME\/emulator/d"' ~/.config/fish/config.fish
-    sed -i 'set -xg PATH "$ANDROID_HOME\/platform-tools/d"' ~/.config/fish/config.fish
+    sed -i '/set -x ANDROID_HOME $HOME\/Development\/android/d' ~/.config/fish/config.fish
+    sed -i '/fish_add_path $HOME\/Development\/flutter\/bin/d' ~/.config/fish/config.fish
+    sed -i '/fish_add_path $ANDROID_HOME\/cmdline-tools\/latest\/bin/d' ~/.config/fish/config.fish
+    sed -i '/fish_add_path $ANDROID_HOME\/emulator/d' ~/.config/fish/config.fish
+    sed -i '/fish_add_path $ANDROID_HOME\/platform-tools/d' ~/.config/fish/config.fish
 fi
 
 # Remove the Flutter installation
